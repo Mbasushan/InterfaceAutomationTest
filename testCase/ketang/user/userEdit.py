6d810c9714555c3e40abf3a4bb5b5a720f1fab46
Mbasushan <1701486431@qq.com>
2020-03-30 15:27:22 +0800

    优化：修改用户信息接口调整，开通会员旧接口

--- testCase/ketang/user/userEdit.py
+++ testCase/ketang/user/userEdit.py
@@ -9,29 +9,29 @@ import db_fixture.mysql_db as mySqlConnect
 class UserEdit(unittest.TestCase):
 
     def setUp(self):
-        self.base_url = "http://ke.test.mbalib.com/api/UserEdit"
+        self.base_url = "http://passport.test.mbalib.com/Inside/updateUserInfo"
 
     def test_edit_rname(self):
-        """修改用户信息---名称"""
+        """修改用户信息---姓名"""
         access_token=Token.get_token_login('sxs14','123456')
-        params={'access_token':access_token,'rname':'修改名称'}
+        params={'access_token':access_token,'realname':'修改姓名'}
         respsonse=requests.post(self.base_url,params)
         #查询数据
         rname=select('data_real_name')
-        self.assertEqual(rname,'修改名称')
+        self.assertEqual(rname,'修改姓名')
         #删除数据
-        delete_comment()
+        delete_comment('data_real_name')
 
     def test_edit_job(self):
         """修改用户信息---职务"""
         access_token = Token.get_token_login('sxs14', '123456')
-        params = {'access_token': access_token, 'rname': '测试'}
+        params = {'access_token': access_token, 'job': '测试'}
         respsonse = requests.post(self.base_url, params)
         # 查询数据
-        rname = select('data_position')
+        rname = select('data_job')
         self.assertEqual(rname, '测试')
         # 删除数据
-        delete_comment()
+        delete_comment('data_job')
 
     def test_edit_company(self):
         """修改用户信息---企业"""
@@ -42,7 +42,7 @@ class UserEdit(unittest.TestCase):
         rname = select('data_company')
         self.assertEqual(rname, 'MBA')
         # 删除数据
-        delete_comment()
+        delete_comment('data_company')
 
     def test_edit_area(self):
         """修改用户信息---地区"""
@@ -53,7 +53,9 @@ class UserEdit(unittest.TestCase):
         rname = select('data_province,data_city,data_country')
         self.assertEqual(rname, '350000,350200,350203')
         # 删除数据
-        delete_comment()
+        delete_comment('data_province')
+        delete_comment('data_city')
+        delete_comment('data_country')
 
     def test_edit_noParams(self):
         """修改用户信息---未传参数"""
@@ -68,7 +70,7 @@ class UserEdit(unittest.TestCase):
         params = {'access_token': ''}
         respsonse = requests.post(self.base_url, params)
         result=respsonse.json()
-        self.assertEqual(result['error'], '获取账号信息失败')
+        self.assertEqual(result['error'], '参数错误')
 
 #查询
 def select(params):
@@ -81,21 +83,25 @@ def select(params):
     cs1.execute(query)
     results = cs1.fetchall()
     print(cs1.fetchone())
-    for row in results:
-        province = row[0]
-        city = row[1]
-        country = row[2]
-    result=province+','+city+','+country
+    if len(results[0])!=1:
+        for row in results:
+            province = row[0]
+            city = row[1]
+            country = row[2]
+        result=province+','+city+','+country
+    else:
+        for row in results:
+            result=row[0]
     return result
 
 #删除数据
-def delete_comment():
+def delete_comment(params):
     # 连接数据库
     conn = mySqlConnect.my_db()
     # 获取cursor对象
     cs1 = conn.cursor()
     # 查询主题信息
-    query = "delete FROM mbalib_user_data WHERE data_user_id='6191496'"
+    query = "update mbalib_user_data set "+params+"=''WHERE data_user_id='6191496'"
     try:
         cs1.execute(query)
         conn.commit()
--- testCase/ketang/vip/recharge.py
+++ testCase/ketang/vip/recharge.py
@@ -6,87 +6,87 @@ import testCase.common.getToken as getToken
 import testCase.common.virtualPayment as virtualPayment
 import db_fixture.mysql_db as mySqlConnect
 
-#开通会员
+#开通会员-旧接口
 class Recharge(unittest.TestCase):
 
     def setUp(self):
         self.base_url = "http://ke.test.mbalib.com/vip/recharge"
 
-    def test_getVipConfig_noType(self):
-        """开通会员-未传会员类型"""
-        #paytype:weixin,alipay,mb
-        access_toke=getToken.getToken()
-        params={"paytype":"weixin","access_token":access_toke}
-        response = requests.post(self.base_url,params=params)
-        result = response.json()
-        self.assertEqual(result['error'],'参数错误')
+    # def test_getVipConfig_noType(self):
+    #     """开通会员-未传会员类型"""
+    #     #paytype:weixin,alipay,mb
+    #     access_toke=getToken.getToken()
+    #     params={"paytype":"weixin","access_token":access_toke}
+    #     response = requests.post(self.base_url,params=params)
+    #     result = response.json()
+    #     self.assertEqual(result['error'],'参数错误')
 
-    def test_getVipConfig_noPayType(self):
-        """开通会员-未传支付类型"""
-        #paytype:weixin,alipay,mb
-        access_toke=getToken.getToken()
-        params={"type":"ketang_vip_year","access_token":access_toke}
-        response = requests.post(self.base_url,params=params)
-        result = response.json()
-        print(result)
-        self.assertEqual(result['state'],'success')
-        print("未传支付类型，则默认为微信支付，所以发起开通会员成功")
-
-    def test_getVipConfig_noAccessToke(self):
-        """开通会员-未传用户令牌"""
-        #paytype:weixin,alipay,mb
-        params={"type":"ketang_vip_year","paytype":'weixin',"access_token":''}
-        response = requests.post(self.base_url,params=params)
-        result = response.json()
-        print(result)
-        self.assertEqual(result['error'],'获取账号信息失败')
-
-    def test_getVipConfig_weixin(self):
-        """开通会员-微信支付"""
-        #paytype:weixin,alipay,mb
-        print("发起订单")
-        access_toke = getToken.getToken()
-        params={"type":"ketang_vip_year","paytype":'weixin',"access_token":access_toke}
-        response = requests.post(self.base_url,params=params)
-        result = response.json()
-        print(result)
-        self.assertEqual(result['state'],'success')
-        trade=result['order_number']
-        fee=getFee(str(result['id']))
-        print(fee)
-        #虚拟支付接口
-        virtualPayment.test_virtualPaymentKetang(self,trade,fee)
-
-
-    def test_getVipConfig_alipay(self):
-        """开通会员-支付宝"""
-        #paytype:weixin,alipay,mb
-        print("发起订单")
-        access_toke = getToken.getToken()
-        params={"type":"ketang_vip_year","paytype":'alipay',"access_token":access_toke}
-        response = requests.post(self.base_url,params=params)
-        result = response.json()
-        print(result)
-        self.assertEqual(result['state'],'success')
-        trade = result['order_number']
-        fee = getFee(str(result['id']))
-        print(fee)
-        # 虚拟支付接口
-        virtualPayment.test_virtualPaymentKetang(self, trade, fee)
-
-    def test_getVipConfig_mb(self):
-        """开通会员-M币"""
-        #paytype:weixin,alipay,mb
-        print("发起订单")
-        access_toke = getToken.getToken()
-        params={"type":"ketang_vip_year","paytype":'mb',"access_token":access_toke}
-        response = requests.post(self.base_url,params=params)
-        result = response.json()
-        print(result)
-        if 'state' in result:
-            self.assertEqual(result['state'], 'success')
-        else:
-            self.assertEqual(result['error'],'余额不足')
+    # def test_getVipConfig_noPayType(self):
+    #     """开通会员-未传支付类型"""
+    #     #paytype:weixin,alipay,mb
+    #     access_toke=getToken.getToken()
+    #     params={"type":"ketang_vip_year","access_token":access_toke}
+    #     response = requests.post(self.base_url,params=params)
+    #     result = response.json()
+    #     print(result)
+    #     self.assertEqual(result['state'],'success')
+    #     print("未传支付类型，则默认为微信支付，所以发起开通会员成功")
+    #
+    # def test_getVipConfig_noAccessToke(self):
+    #     """开通会员-未传用户令牌"""
+    #     #paytype:weixin,alipay,mb
+    #     params={"type":"ketang_vip_year","paytype":'weixin',"access_token":''}
+    #     response = requests.post(self.base_url,params=params)
+    #     result = response.json()
+    #     print(result)
+    #     self.assertEqual(result['error'],'获取账号信息失败')
+    #
+    # def test_getVipConfig_weixin(self):
+    #     """开通会员-微信支付"""
+    #     #paytype:weixin,alipay,mb
+    #     print("发起订单")
+    #     access_toke = getToken.getToken()
+    #     params={"type":"ketang_vip_year","paytype":'weixin',"access_token":access_toke}
+    #     response = requests.post(self.base_url,params=params)
+    #     result = response.json()
+    #     print(result)
+    #     self.assertEqual(result['state'],'success')
+    #     trade=result['order_number']
+    #     fee=getFee(str(result['id']))
+    #     print(fee)
+    #     #虚拟支付接口
+    #     virtualPayment.ketang_pay(self,trade,fee)
+    #
+    #
+    # def test_getVipConfig_alipay(self):
+    #     """开通会员-支付宝"""
+    #     #paytype:weixin,alipay,mb
+    #     print("发起订单")
+    #     access_toke = getToken.getToken()
+    #     params={"type":"ketang_vip_year","paytype":'alipay',"access_token":access_toke}
+    #     response = requests.post(self.base_url,params=params)
+    #     result = response.json()
+    #     print(result)
+    #     self.assertEqual(result['state'],'success')
+    #     trade = result['order_number']
+    #     fee = getFee(str(result['id']))
+    #     print(fee)
+    #     # 虚拟支付接口
+    #     virtualPayment.ketang_pay(self, trade, fee)
+    #
+    # def test_getVipConfig_mb(self):
+    #     """开通会员-M币"""
+    #     #paytype:weixin,alipay,mb
+    #     print("发起订单")
+    #     access_toke = getToken.getToken()
+    #     params={"type":"ketang_vip_year","paytype":'mb',"access_token":access_toke}
+    #     response = requests.post(self.base_url,params=params)
+    #     result = response.json()
+    #     print(result)
+    #     if 'state' in result:
+    #         self.assertEqual(result['state'], 'success')
+    #     else:
+    #         self.assertEqual(result['error'],'余额不足')
 
 
 
