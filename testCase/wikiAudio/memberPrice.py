562b073353730b0b1bba09a9cf9c63ceee847fe9
Mbasushan <1701486431@qq.com>
2020-03-30 17:31:13 +0800

    优化：调整大咖会员配置，添加请求头信息，该请求有版本号控制

--- testCase/wikiAudio/memberPrice.py
+++ testCase/wikiAudio/memberPrice.py
@@ -25,7 +25,9 @@ class MemberPrice(unittest.TestCase):
     def test_getMemberPrice_ios_wiki(self):
         """大咖会员开通配置-客户端:wiki"""
         origin='wiki'
-        response=requests.get(self.base_url,params={'origin':origin})
+        header = {'User-Agent': 'MBALIB-WIKI-APP/6.9.7(iPhone 8 Plus;iOS 12.3.2;mbalibnormal;zh-cn;Build/374;Device/7740195D-701F-4026-9A05-C05954C2AFEC;)'}
+        params = {'origin':origin}
+        response = requests.post(self.base_url, data=params, headers=header)
         result=response.json()
         self.assertEqual(result['state'],'success')
         print("客户端：ios-普通版的会员配置项数量：", len(result['data']))
@@ -35,7 +37,9 @@ class MemberPrice(unittest.TestCase):
     def test_getMemberPrice_ios_wikifresh(self):
         """大咖会员开通配置-客户端:wikifresh"""
         origin='wikifresh'
-        response=requests.get(self.base_url,params={'origin':origin})
+        header = {'User-Agent': 'MBALIB-WIKI-APP/6.9.7(iPhone 8 Plus;iOS 12.3.2;mbalibfresh;zh-cn;Build/374;Device/7740195D-701F-4026-9A05-C05954C2AFEC;)'}
+        params = {'origin': origin}
+        response = requests.post(self.base_url, data=params, headers=header)
         result=response.json()
         self.assertEqual(result['state'],'success')
         print("客户端：ios-专业版的会员配置项数量：", len(result['data']))
@@ -45,7 +49,9 @@ class MemberPrice(unittest.TestCase):
     def test_getMemberPrice_android(self):
         """大咖会员开通配置-客户端:android"""
         origin='android'
-        response=requests.get(self.base_url,params={'origin':origin})
+        header = {'User-Agent': 'MBALIB-WIKI-APP/6.9.7(CLT-AL00; Android 9;zh-cn;Build/113;Device/ffffffff-ad36-96e3-ad36-96e300000000;)'}
+        params = {'origin': origin}
+        response = requests.post(self.base_url, data=params, headers=header)
         result=response.json()
         self.assertEqual(result['state'],'success')
         print("客户端：Android的会员配置项数量：",len(result['data']))
@@ -54,7 +60,10 @@ class MemberPrice(unittest.TestCase):
 
     def test_getMemberPrice_noOrigin(self):
         """大咖会员开通配置-未传客户端"""
-        response=requests.get(self.base_url)
+        header = {
+            'User-Agent': 'MBALIB-WIKI-APP/6.9.7(CLT-AL00; Android 9;zh-cn;Build/113;Device/ffffffff-ad36-96e3-ad36-96e300000000;)'}
+        params = {'origin': ''}
+        response = requests.post(self.base_url, data=params, headers=header)
         result=response.json()
         self.assertEqual(result['state'],'success')
         print(len(result['data']))
@@ -64,7 +73,7 @@ class MemberPrice(unittest.TestCase):
 #获取会员配置在数据库中的总数
 def count(origin):
     #获取当前时间
-    time=datetime.datetime.now().strftime('%Y-%m-%d hh:mm:ss')
+    time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     # 连接数据库
     conn = mySqlConnect.my_db()
     # 获取cursor对象
@@ -76,7 +85,7 @@ def count(origin):
     elif origin=="wiki" or origin=='wikifresh':
         query="SELECT COUNT(*) FROM wiki_audio_member_config WHERE mc_state=1 AND mc_client=2 and mc_endtime>='"+time+"'"
     elif origin=='android':
-        query="SELECT COUNT(*) FROM wiki_audio_member_config WHERE mc_state=1 AND mc_client=3 and mc_endtime>='"+time+"'"
+        query = "SELECT COUNT(*) FROM wiki_audio_member_config WHERE mc_state=1 AND mc_client=3 and mc_endtime>='" + time + "'"
     elif origin=='h5':
         query = "SELECT COUNT(*) FROM wiki_audio_member_config WHERE mc_state=1 AND mc_client=4 and mc_endtime>='"+time+"'"
     cs1.execute(query)
--- testCase/wikiAudio/payVip.py
+++ testCase/wikiAudio/payVip.py
@@ -12,7 +12,7 @@ class AudioVip_pay(unittest.TestCase):
 
     def setUp(self):
         self.base_url="http://passport.test.mbalib.com/api2/RechargeWealth"
-        self.access_token=Token.get_token_login('sxs14','123456')
+        self.access_token=Token.getToken()
 
     def test_h5_audioVip_pay(self):
         """h5开通大咖VIP支付"""
@@ -27,6 +27,8 @@ class AudioVip_pay(unittest.TestCase):
             trade=result['order_number']
             fee=result['amount']
             virtualPayment.audio_pay(self,trade,fee)
+        else:
+            print("大咖会员配置为空")
 
 
     def test_ios_audioVip_pay(self):
@@ -36,13 +38,15 @@ class AudioVip_pay(unittest.TestCase):
         if memberPrices!=None:
             header = {'User-Agent': 'MBALIB-WIKI-APP/6.7.1(iPhone 8 Plus;iOS 12.3.2;mbalibnormal;zh-cn;Build/374;Device/7740195D-701F-4026-9A05-C05954C2AFEC;)'}
             params={'access_token':self.access_token,'member':memberPrices[0]['key'],'amount':memberPrices[0]['price'],'key':memberPrices[0]['key'],'origin':'wiki','type':'audio_member','pay_type':'weixin'}
-            response=requests.post(self.base_url,headers=header,data=params)
+            response=requests.post(self.base_url,data=params,headers=header)
             result=response.json()
             self.assertEqual(result['state'],'success')
             #虚拟支付
             trade=result['order_number']
             fee=memberPrices[0]['price']
             virtualPayment.audio_pay(self,trade,fee)
+        else:
+            print("大咖会员配置为空")
 
     def test_iosFresh_audioVip_pay(self):
         """ios专业版-开通大咖VIP支付"""
@@ -58,18 +62,22 @@ class AudioVip_pay(unittest.TestCase):
             trade=result['order_number']
             fee=memberPrices[0]['price']
             virtualPayment.audio_pay(self,trade,fee)
+        else:
+            print("大咖会员配置为空")
 
     def test_Android_audioVip_pay(self):
         """Android-开通大咖VIP支付"""
         #获取商品项
         memberPrices=getVipPrices.getVip_prices(self,'android')
         if memberPrices!=None:
-            header = {'User-Agent': 'MBALIB-WIKI-APP/6.6.0(CLT-AL00; Android 9;zh-cn;Build/113;Device/ffffffff-ad36-96e3-ad36-96e300000000;)'}
+            header = {'User-Agent': 'MBALIB-WIKI-APP/6.9.7(CLT-AL00; Android 9;zh-cn;Build/113;Device/ffffffff-ad36-96e3-ad36-96e300000000;)'}
             params={'access_token':self.access_token,'member':memberPrices[0]['key'],'amount':memberPrices[0]['price'],'key':memberPrices[0]['key'],'origin':'androidwiki','type':'audio_member','pay_type':'weixin'}
-            response=requests.post(self.base_url,headers=header,data=params)
+            response=requests.post(self.base_url,data=params,headers=header)
             result=response.json()
             self.assertEqual(result['state'],'success')
             #虚拟支付
             trade=result['order_number']
             fee=memberPrices[0]['price']
             virtualPayment.audio_pay(self,trade,fee)
+        else:
+            print("Android未有大咖讲百科VIP配置")
